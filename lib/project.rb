c8205594aed3e58c70aa574afbcd14b8ab2b532d
Erin Ponsonby <erinponsonby@gmail.com>
2019-05-19 17:36:39 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -8,7 +8,7 @@ class Project
   
   def backers=(backers) #project belongs to a backer
     @backers = backers
-    backer.back_project(self) unless backers.backed_projects.include?(self)
+    backer.back_project(project) unless backers.backed_projects.include?(self)
   end
   
   def add_backer(backer_instance)
