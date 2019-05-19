b0a7b4f6e7cc4d02300a0b3f75db4bbf0142126a
Erin Ponsonby <erinponsonby@gmail.com>
2019-05-19 17:22:11 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -13,9 +13,6 @@ class Project
   
   def add_backer(backer_instance)
     @backers << backer_instance
-    # backer_instance.back_project(self)
-    #   while !@backed_projects.include?(self)
-    # end
   end
   
 end
